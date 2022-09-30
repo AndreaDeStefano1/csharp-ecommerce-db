131f9164d104100f4856137802783078af066875
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-09-30 16:43:11 +0200

    UPDATE

--- Customer.cs
+++ Customer.cs
@@ -9,5 +9,56 @@
 
         List<Order> Orders { get; set; }
 
+
+        public static Customer CreateCustomer(string name, string surname, string email)
+        {
+            Customer nC = new Customer();
+
+            using (OrderContext db = new OrderContext())
+            {
+                nC.Name = name;
+                nC.Surname = surname;
+                nC.Email = email;
+
+                db.Add(nC);
+                db.SaveChanges();
+            }
+            return nC;
+        }
+
+        public static void UpdateCustomer(string email)
+        {
+
+            using (OrderContext db = new OrderContext())
+            {
+                Customer c = (from s in db.Customers
+                               where s.Email == email
+                               select s).First<Customer>();
+
+
+                c.Name = ValueChange(c.Name); //QUI NON MI VEDEVA LA FUNZIONE SE NON STATICA
+                c.Surname = ValueChange(c.Surname);
+                c.Email = ValueChange(c.Email);
+
+                db.SaveChanges();
+            }
+        }
+
+        public string ToString(Customer c)
+        {
+            
+            return $"----\n----\nNome: {c.Name}\nCognome: {c.Surname}\nEmail: c{Email}\n----";
+        }
+        
+        public static string ValueChange(string value)
+        {
+            Console.WriteLine($"Vuoi modificare il nome? [{value}]");
+            string newValue = Console.ReadLine();// QUI VORREI USARE IL MIO STRINGINPUT() CHE SI TROVA IN PROGRAM
+            if (newValue != "")
+            {
+                return newValue;
+            }
+            return value;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,20 +6,59 @@ static void MenuOptions()
     Console.WriteLine("1-Employee\n2-Customer\n3-Product");
 }
 
-using(OrderContext db = new OrderContext())
-{
-    Customer nC = new Customer();
-    nC.Name = StringInput("\nNome:");
-    nC.Surname = StringInput("\nCognome:");
-    nC.Email = StringInput("\nEmail:");
 
-    db.Add(nC);
-    db.SaveChanges();
-}
 
 
 static string StringInput(string message)
 {
     Console.WriteLine(message);
     return Console.ReadLine();
+}
+
+
+
+bool exit = false;
+
+while (!exit)
+{
+    MenuOptions();
+
+    int choice = Convert.ToInt32(StringInput("Cosa vuoi fare:"));
+    switch (choice)
+    {
+
+        case 1:
+            Console.WriteLine("1-Aggiungi nuovo\n2-Modifica\n3-Visualizza\n4-Elimina");
+            bool CusExit = false;
+            while (!CusExit)
+            {
+                int CusChoice = Convert.ToInt32(StringInput("Cosa vuoi fare:"));
+                switch (CusChoice)
+                {
+                    case 1:
+                        Customer nC = Customer.CreateCustomer(StringInput("Nome:"), StringInput("Cognome"), StringInput("Email:"));
+                        Console.WriteLine(nC.ToString(nC));
+                        break;
+
+                    case 2:
+                        Customer.UpdateCustomer(StringInput("Inserisci la mail del cliente da modificare"));
+                        Console.WriteLine("modificato");
+                        break;
+
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        CusExit = true;
+                        break;
+                }
+            }
+
+            break;
+
+
+        default:
+            break;
+    }
 }
